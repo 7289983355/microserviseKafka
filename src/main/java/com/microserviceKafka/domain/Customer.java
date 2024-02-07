b55ddd036742fd66edfe59ff7f5e3b8c38b58f93
dharmesh <dharmeshkumar016@gmail.com>
2024-02-07 14:24:40 +0530

    entity changed add

---
+++ src/main/java/com/microserviceKafka/domain/Customer.java
@@ -0,0 +1,28 @@
+package com.microserviceKafka.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthDate;
+	private String emailAddress;
+	
+
+}
